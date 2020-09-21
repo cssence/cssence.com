a9d5045624af5b93ce1222b737b495648a921d8c
Matthias Zöchling <github@cssence.com>
2020-09-21 08:17:32 +0200

    CodePen script improvements

--- assets/js/codepen.js
+++ assets/js/codepen.js
@@ -1,15 +1,22 @@
 /* cssence.com codepen.io-enhancer */
 (function(document) {
 	'use strict';
-	if ('querySelector' in document) {
-		var help = document.querySelector('[rel="help"][href*="cssence.com"]');
-		if (help) {
-			var a = document.createElement('a');
-			a.href = help.href;
-			a.rel = 'help';
-			a.target = '_blank';
-			a.textContent = 'Learn more';
-			document.body.appendChild(a);
-		}
+	if (!('querySelector' in document)) return;
+	var script = document.querySelector('script[src^="https://cssence.com/js/codepen"]');
+	var helpUrl = script.src.split('#')[1];
+	if (!helpUrl) {
+		console.warn('Added "Learn more" script without help URL.');
+		return;
 	}
+	var style = document.createElement('link');
+	style.rel = 'stylesheet';
+	style.href = 'https://cssence.com/css/codepen.css';
+	document.head.appendChild(style);
+	var anchor = document.createElement('a');
+	anchor.rel = 'help';
+	anchor.href = 'https://cssence.com' + helpUrl;
+	anchor.target = '_blank';
+	anchor.setAttribute('hidden', '');
+	anchor.textContent = 'Learn more';
+	document.body.appendChild(anchor);
 }(document));
--- assets/js/ctm.js
+++ assets/js/ctm.js
@@ -1,48 +1,50 @@
 /* cssence.com init script (mustard cutter, async asset loader) */
 (function(window, document) {
+	'use strict';
 	try {
 		var addStyle = function (href) {
-			var stylesheet = document.createElement("link");
-			stylesheet.setAttribute("rel", "stylesheet");
-			stylesheet.setAttribute("media", "screen");
+			var stylesheet = document.createElement('link');
+			stylesheet.setAttribute('rel', 'stylesheet');
+			stylesheet.setAttribute('media', 'screen');
 			stylesheet.href = href;
-			var insertAfter = document.querySelector("style.page");
+			var insertAfter = document.querySelector('style.page');
 			insertAfter.parentNode.insertBefore(stylesheet, insertAfter.nextSibling);
 		};
-		if (!window.CSS.supports("--:1") || window.location.search === "?ie") {
-			throw "No CSS custom properties.";
+		if (!window.CSS.supports('--:1') || window.location.search === '?ie') {
+			console.info('No further progressive enhancement.');
+			return;
 		}
-		document.addEventListener("DOMContentLoaded", function() {
-			if (document.querySelectorAll("code[class]").length) {
+		document.addEventListener('DOMContentLoaded', function() {
+			if (document.querySelectorAll('code[class]').length) {
 				/* loadJS: load a JS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. */
 				var loadJS = function (src, cb) {
-					var ref = window.document.getElementsByTagName("script")[0];
-					var script = window.document.createElement("script");
+					var ref = window.document.getElementsByTagName('script')[0];
+					var script = window.document.createElement('script');
 					script.src = src;
 					script.async = true;
 					ref.parentNode.insertBefore(script, ref);
-					if (cb && typeof(cb) === "function") {
+					if (cb && typeof(cb) === 'function') {
 						script.onload = cb;
 					}
 					return script;
 				};
-				addStyle("/css/prism.css");
-				loadJS("/js/prism.min.js", function () {
+				addStyle('/css/prism.css');
+				loadJS('/js/prism.min.js', function () {
 					Prism.highlightAll();
 				});
 			}
 		});
 	} catch (err) {
 		console.log(err);
-		var styles = document.querySelector("head").querySelectorAll("link[rel=stylesheet],style");
+		var styles = document.querySelector('head').querySelectorAll('link[rel="stylesheet"],style');
 		for (var i = styles.length - 1; i >= 0; --i) {
 			var style = styles[i];
-			if (style.tagName.toLowerCase() === "link") {
+			if (style.tagName.toLowerCase() === 'link') {
 				style.parentNode.removeChild(style);
 			} else {
-				style.removeAttribute("media");
+				style.removeAttribute('media');
 			}
 		}
-		addStyle("/css/ie.css");
+		addStyle('/css/ie.css');
 	}
 }(window, document));
