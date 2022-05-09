508fd086622ad08356c9c6226ea71b679ccb7051
Matthias Zöchling <github@cssence.com>
2022-05-09 15:44:02 +0200

    v4.11.0 move .codepen before #updates and #footnotes

--- ssg/render/augment/codepen.mjs
+++ ssg/render/augment/codepen.mjs
@@ -11,7 +11,16 @@ const modify = (content, meta) => {
 		`<p><a href="${meta.page.alternateUrl}">Visit CodePen to see this&nbsp;example in&nbsp;action.</a></p>`,
 		'</aside>'
 	];
-	const insertBefore = content.indexOf('<h2 id="comments">Comments</h2>') - 1;
+	const hasUpdates = content.indexOf('<h2 id="updates">Updates</h2>');
+	const hasFootnotes = content.indexOf('<h2>Footnotes</h2>');
+	let insertBefore = content.indexOf('<h2 id="comments">Comments</h2>') - 2;
+	for (let i = insertBefore - 1; content[i] !== '</header>'; i -= 1) {
+		if (content[i].startsWith('<h2 id="updates"') || content[i] === '<h2>Footnotes</h2>') {
+			insertBefore = i;
+		} else if (content[i].startsWith('<h2')) {
+			break;
+		}
+	}
 	content.splice(insertBefore, 0, aside.join('\n'));
 
 };
