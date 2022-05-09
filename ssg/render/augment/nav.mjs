4930f0ec3af7bf07276e54aa6a5038584462151c
Matthias Zöchling <github@cssence.com>
2022-05-09 15:30:11 +0200

    v4.10.2 hide logo SVG in plain HTML

--- ssg/render/augment/nav.mjs
+++ ssg/render/augment/nav.mjs
@@ -3,7 +3,7 @@
 const modify = (content, meta) => {
 
 	const navItems = [
-		{url: '/', label: 'Home', className: 'c-default', suffix: `<span class="${meta.page.className}" aria-hidden="true" data-hl="Home"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="-8 -8 112 112" focusable="false"><path fill="#fff" d="M48 8C26 8 8 26 8 48s18 40 40 40c14 0 26.2-7 33.4-18L69.7 58.4c-4 8-12 13.7-21.7 13.7-13.3 0-24-10.7-24-24s10.7-24 24-24c9.6 0 17.8 5.6 21.7 13.7L81.4 26C74.2 15 62 8 48 8z"/><path fill="#d34b6c" d="M58.3 26.3l-32 32c2.4 5 6.4 9 11.4 11.4l32-32c-2.4-5-6.4-9-11.4-11.4z"/></svg></span>`},
+		{url: '/', label: 'Home', className: 'c-default', suffix: `<span class="${meta.page.className}" hidden aria-hidden="true" data-hl="Home"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="-8 -8 112 112"><path fill="#fff" d="M48 8C26 8 8 26 8 48s18 40 40 40c14 0 26.2-7 33.4-18L69.7 58.4c-4 8-12 13.7-21.7 13.7-13.3 0-24-10.7-24-24s10.7-24 24-24c9.6 0 17.8 5.6 21.7 13.7L81.4 26C74.2 15 62 8 48 8z"/><path fill="#d34b6c" d="M58.3 26.3l-32 32c2.4 5 6.4 9 11.4 11.4l32-32c-2.4-5-6.4-9-11.4-11.4z"/></svg></span>`},
 		{url: '/articles/', label: 'Articles', className: 'c-article'},
 		{url: '/notes/', label: 'Notes', className: 'c-note'},
 		{url: '/about/', label: 'About', className: 'c-about'},
