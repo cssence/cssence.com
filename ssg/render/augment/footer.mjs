397c9fafeb2f7ad12d7182f1051946b443176fa5
Matthias Zöchling <github@cssence.com>
2025-03-11 15:06:09 +0100

    v4.64.0 v5 preparaton pt.1

--- ssg/render/augment/footer.mjs
+++ ssg/render/augment/footer.mjs
@@ -4,7 +4,7 @@ const modify = (content, meta) => {
 
 	const footer = [
 		'<footer class="imprint">',
-		`<p><span>© 2010–${meta.date.year} <a href="/about/matt/">Matthias Zöchling.</a>  </span><span><a href="/imprint/">Imprint</a>  <a href="#navigation">Menu<span aria-hidden="true" data-hl="Menu"></span></a>  <a href="/settings/">Settings<span aria-hidden="true" data-hl="Settings"></span></a>  <a href="/webrings/">Webrings</a>  <a href="/elsewhere/">Elsewhere</a>  </span><a href="/rss/">Subscribe (RSS)</a>  <a href="#top">Back to top</a></p>`,
+		`<p><span>© 2010–${meta.date.year} <a href="/about/matt/">Matthias Zöchling.</a>  </span><span><a href="/imprint/">Imprint</a>  <a href="/webrings/">Webrings</a>  <a href="/elsewhere/">Elsewhere</a>  <a href="#navigation">Menu<span aria-hidden="true" data-hl="Menu"></span></a>  <a href="/settings/">Settings<span aria-hidden="true" data-hl="Settings"></span></a>  </span><a href="/rss/">RSS-Feeds</a>  <a href="#top">Back to top</a></p>`,
 		'</footer>'
 	];
 
