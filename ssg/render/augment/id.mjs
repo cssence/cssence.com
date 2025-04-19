4a17ceeea4ed25213148751abd8f1e1ec0e11088
Matthias Zöchling <github@cssence.com>
2025-04-19 11:01:51 +0200

    v4.75.0 headings with tabindex=-1

--- ssg/render/augment/id.mjs
+++ ssg/render/augment/id.mjs
@@ -1,4 +1,4 @@
-/* Assign IDs, aria-labelledby, and data-title */
+/* Assign IDs, tabindex, aria-labelledby, and data-title */
 
 const modify = (content, meta) => {
 
@@ -13,9 +13,14 @@ const modify = (content, meta) => {
 
 	const stopBefore = content.lastIndexOf('</div>');
 	for (i = content.indexOf('</header>') + 1; i < stopBefore; i += 1) {
+		if (content[i].startsWith('<figure id=')) {
+			content[i] = content[i].replace('>', ' tabindex="-1">'); // focusable ^1
+			continue;
+		}
 		if (!content[i].match(/^<h[1-6]/)) continue; // no heading
 		let text2id = content[i].slice(content[i].indexOf('>') + 1, -'</h#>'.length); // innerText
 		const id = content[i].match(/id="([^"]+)"/)?.[1];
+		content[i] = content[i].replace('>', ' tabindex="-1">'); // focusable ^1
 		if (id) { // already has ID
 			if (content[i].startsWith('<h2 id="updates"') || content[i].startsWith('<h2 id="fns"')) {
 				content[i - 1] = `<section aria-labelledby="${id}"><hr>`;
@@ -29,5 +34,7 @@ const modify = (content, meta) => {
 		content[i] = content[i].replace(tag, `${tag} id="${text2id}"`);
 	}
 };
+
+// ^1 https://brucelawson.co.uk/2024/drive-by-accessibility-tweaks/
 
 export default modify;
